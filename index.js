59870d7b2d33e288e73a5e0a3d18323e2fdafdc2
Iván Hernández <ihdz.ortega@gmail.com>
2019-11-29 10:49:58 +0100

    chore: minor clean up

--- index.js
+++ index.js
@@ -7,14 +7,11 @@ const typeDef = gql`
   type Query
 `;
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({
   typeDefs: [typeDef, bookTypeDef],
   resolvers: [bookResolvers],
 });
 
-// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
